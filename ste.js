8508d7f91a8b2d1eb21f9021ae15355d2819010a
stefanyjs <117385171+stefanyjs@users.noreply.github.com>
2022-12-03 10:30:07 -0300

    Add files via upload

--- ste.js
+++ ste.js
@@ -1,7 +1,6 @@
 function enviar(){
     var nome = document.getElementById("caixa1").value;
-    var nome2 = document.getElementById("caixa2").value;
-    alert("seja bem vindo " + nome + "  " + nome2);
+    alert("seja bem vindo " + nome);
     var nome3 = document.getElementById("nome").value;
-    document.getElementById("saudacao").innerHTML = ("Seja bem vindo, " + nome2);
+    document.getElementById("saudacao").innerHTML = ("Seja bem vindo, " + nome);
 }
